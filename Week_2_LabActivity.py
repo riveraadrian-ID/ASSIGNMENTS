505791d5a1e968988ed1743fddb6489afddadf47
riveraadrian-ID <rivera.adrian00001@gmail.com>
2026-02-13 04:36:03 +0800

    Submit assignment

---
+++ Week_2_LabActivity.py
@@ -0,0 +1,20 @@
+fullName = ('Adrian A. Rivera')
+age = int(20)
+height = float(1.778)
+workStat = False
+
+print("Full Name: ", fullName)
+print(type(fullName))
+print()
+
+print("Age: ", age)
+print(type(age))
+print()
+
+print("Height(Meters): ", height)
+print(type(height))
+print()
+
+print("Currently Working? ", workStat)
+print(type(workStat))
+print()
